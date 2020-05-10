1777e99f2b1bfdeaed4ff4b5a790fc371305e8c1
jimtie <jimmychen.xin@gmail.com>
2020-05-10 16:42:14 -0700

    for heroku

---
+++ src/components/DeleteCreditForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles.css'
+
+function DeleteCreditForm(props){
+  return(
+    <p className="mt-4 align-center">Are you sure you want to delete this credit card?
+    </p>
+  )
+}
+
+export default DeleteCreditForm;
